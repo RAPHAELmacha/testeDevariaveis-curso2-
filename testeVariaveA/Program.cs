e43c2b326e5530e1c861113aa21340585b50fe29
RAPHAELmacha <136842294+RAPHAELmacha@users.noreply.github.com>
2025-03-06 19:22:56 -0300

    TesteVariavel

---
+++ testeVariaveA/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program 
+{
+    public class Program
+    {
+        static void Main(string[] args) 
+        {
+            int numero1 = 4;
+            int numero2 = 10;
+            int numero3 = 5;
+
+            var soma = numero1 + numero2 + numero3;
+
+            Console.WriteLine(soma);
+
+            var quadrado1 = new Qadrado(); //cria um quadrado
+            quadrado1.lado = 15; //medida do quadrado
+            var quadrado2= quadrado1;
+            quadrado2.lado=11;
+
+            Console.WriteLine(quadrado1.lado); //quadrado 1 deve ter
+            Console.WriteLine(quadrado2.lado); 
+
+        }
+
+        class Qadrado
+        {
+            public int lado;
+        }
+        
+    }
+}
